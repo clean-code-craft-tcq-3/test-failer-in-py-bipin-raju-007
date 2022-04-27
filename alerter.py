7ea7c78a0a7e407b91b05dce2187e5c34402668b
Bipin Raju (MS/ESZ2-XC) <raju.bipin@in.bosch.com>
2022-04-27 15:42:33 +0530

    Updated the test cases for proper testing

--- alerter.py
+++ alerter.py
@@ -1,16 +1,18 @@
 alert_failure_count = 0
+is_network_down = False
 
-def network_alert_stub(celcius):
-    print(f'ALERT: Temperature is {celcius} celcius')
-    # Return 200 for ok
-    # Return 500 for not-ok
-    # stub always succeeds and returns 200
+
+def network_alert_stub(celsius):
+    print(f'ALERT: Temperature is {celsius} celsius')
+    if is_network_down:
+        return 500
     return 200
 
-def alert_in_celcius(farenheit):
-    celcius = (farenheit - 32) * 5 / 9
-    returnCode = network_alert_stub(celcius)
-    if returnCode != 200:
+
+def alert_in_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    return_code = network_alert_stub(celsius)
+    if return_code != 200:
         # non-ok response is not an error! Issues happen in life!
         # let us keep a count of failures to report
         # However, this code doesn't count failures!
@@ -19,7 +21,10 @@ def alert_in_celcius(farenheit):
         alert_failure_count += 0
 
 
-alert_in_celcius(400.5)
-alert_in_celcius(303.6)
+alert_in_celsius(400.5)
+is_network_down = True
+alert_in_celsius(303.6)
+
 print(f'{alert_failure_count} alerts failed.')
+assert(alert_failure_count == 1)
 print('All is well (maybe!)')
--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,24 @@
+from color_map import *
 
-def print_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-    for i, major in enumerate(major_colors):
-        for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+
+
+def get_num_of_color_mappings():
     return len(major_colors) * len(minor_colors)
 
 
-result = print_color_map()
-assert(result == 25)
+def get_color_map():
+    color_map = ""
+    for i, major in enumerate(major_colors):
+        for j, minor in enumerate(minor_colors):
+            color_map = color_map + f'{i * 5 + j + 1} | {major} | {minor}' + "\n"
+    return color_map
+
+
+color_map_str = get_color_map()
+num_of_color_maps = get_num_of_color_mappings()
+print(color_map_str)
+assert(num_of_color_maps == 25)
+assert(color_map_str == correct_color_map)
 print("All is well (maybe!)\n")
